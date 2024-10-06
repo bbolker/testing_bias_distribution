782498ccb7bf28f988c8ae8ced0fcbc81d20a1c4
Richard Zhao <134243368+RichardSichengZhao@users.noreply.github.com>
2024-10-06 19:02:31 -0400

    Codes for generating figures
    Codes that being used to generate https://github.com/bbolker/testing_bias_distribution/blob/72b36a62b5999ab4206887887770dc20b4bb8d86/Test%20positivity%20vs%20test%20proportion%26phi%26inc.png.

---
+++ Expected Test positivity figure.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(ggplot2); theme_set(theme_bw())
+library(viridis)
+library(bbmle)
+source("prop_pos_test_new.R")
+
+dd <- (expand.grid(test_prop=c(0.001,0.0012,0.0015,0.002,0.005,0.01,0.05,0.1,0.25),
+                   phi=seq(from=0.01,to=0.99,by=0.01),
+                   inc=c(0.001,0.005,0.01,0.02,0.05,0.10,0.25,0.5))
+       %>% as_tibble()
+       %>% mutate(pos_prop=prop_pos_test_new(inc,test_prop,phi,method="log"))
+       # ## issues with pivot_longer? use gather() instead
+       # ## "Error: `spec` must have `.name` and `.value` columns"
+       # ## FIXME: figure out what's going on here?
+       # %>% gather("var","value",inc,test,phi,pos_prop)
+)
+# dd
+
+print(ggplot(dd,aes(phi,pos_prop,col=log10(inc),group=test_prop))
+      + geom_point(size=0.5)
+      + facet_wrap(~test_prop,scale="free",labeller = label_both)
+      #+ scale_y_log10()
+      + scale_colour_viridis_c()
+)
+
