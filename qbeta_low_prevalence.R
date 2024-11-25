22ca5889c8e83e819a4c4bebbd50e3c9f1e5c9a1
Richard Zhao <richardsichengzhao@gmail.com>
2024-11-25 14:10:09 -0500

    qbeta further investigation

--- qbeta_low_prevalence.R
+++ qbeta_low_prevalence.R
@@ -6,24 +6,51 @@ source("testing_funs.R")
 log(qbeta(0.2,1,2,lower.tail = FALSE)+qbeta(0.2,2,1))
 
 
-prop_pos_test_new(0.001,0.4,0.99,method="est",debug = T)
+prop_pos_test_new(0.001,0.571,0.99,method="est",debug = T)
 prop_pos_test_new(0.001,0.572,0.99,method="est",debug = T)
 
 
+plot(seq(0,1,0.001),dbeta(seq(0,1,0.001),a,b),type="l")
+
+### Test Ben's Inverse idea.
+# Q(1-t;a,b)
+lwr_test <- qbeta(0.3,3,5,lower.tail = FALSE)
+lwr_test
+
+# lwr_test + qbeta(0.7,3,5)
+pbeta(lwr_test,3,5)
+
+# Q(t;b,a)
+lwr_inv_test <- qbeta(1-0.3,5,3,lower.tail = FALSE)
+lwr_inv_test
+
+lwr_inv_test+lwr_test
+# Q(t;b,a)+Q(1-t;a,b)=1 (lower tail also works)
+
+
 Phi <- 0.99
-i <- 0.001
-t <- 0.57
+i <- 0.005
+t <- 0.1
 phi <- -log(1-Phi)
 a <- i/phi
 b <- (1-i)/phi
 
-plot(seq(0,1,0.001),dbeta(seq(0,1,0.001),a,b),type="l")
-
-
+### The qbeta provide a wrong value
 lwr <- qbeta(t,a,b,lower.tail=FALSE)
 lwr
 pbeta(lwr,a,b,lower.tail = FALSE)
 
+lwr_inv <-qbeta(1-t,b,a,lower.tail = FALSE)
+lwr_inv
+log(1-lwr_inv)
+# Still, inverse does not provide a reasonable value
+
+prop_pos_test_new(i,t,Phi,method="est",debug = T)
+
+
+
+
+### Direct linear approx: Failed
 g <- function(l,a,b){
   l^a/(a*(a+1))*(a+1-l*(b-1)*(a))
 }
@@ -50,7 +77,7 @@ fig_lwr_heatmap <- (
   + scale_fill_viridis_c()
   + ggtitle("qbeta for prev=0.001 as a function of phi and test proportion")
 )
-
+fig_lwr_heatmap
 ggsave("qbeta_heatmap_low_prevalence.png",plot=fig_lwr_heatmap, path = "./pix", width=3200,height=1800,units="px")
 
 
