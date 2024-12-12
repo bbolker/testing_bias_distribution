36effff80d89ac8bbb806a760f798d84ea17c348
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-12-12 15:02:00 -0500

    modified:   OR_Sim.R (JD tried a bunch of stuff that we might reverse)

--- OR_Sim.R
+++ OR_Sim.R
@@ -16,9 +16,9 @@ Phi <- (T_Y/(1-T_Y))/B
 print(B)
 print(Phi)
 
-pY_0 <- 1e-4
+Y_0 <- 1e-4
 N <- 1e6
-NY_0 <- N*pY_0
+NY_0 <- N*Y_0
 
 r <- log(2)/3
 tmax <- 19
@@ -27,14 +27,15 @@ pts <- length(t)
 
 ## Simulate the data
 dat <- tibble(t=t
-	, pY = pmin(pY_0*exp(r*t), 1)
-	, NY = rbinom(pts, N, pY)
+	, Y = pmin(Y_0*exp(r*t), 1)
+	## , NY = rbinom(pts, N, Y)
+	, NY = round(N*Y)
 	, posTests = rbinom(pts, NY, T_Y)
 	, negTests = rbinom(pts, N-NY, T_B)
 )
 
 long_dat <- (dat
-	|> select(-pY)
+	|> select(-Y)
 	|> pivot_longer(-t)
 )
 
@@ -45,52 +46,36 @@ long_dat <- (dat
 # )
 
 ### function to calculate negative log-likelihood:
-LL <- function(B,Phi,pY_0,r,dat,N,tmax){
+nll <- function(B,Phi,logY_0,r,dat,N,tmax){
+	Y_0 <- exp(logY_0)
   T_B <- B/(1+B)
   T_Y <- B*Phi/(1+B*Phi)
   t <- c(0:tmax)
   pts <- length(t)
+  Ynull = Y_0*exp(r*t) ## Going to truncate here for now; should we change sim instead?
   ### simulated time series
   sim <- tibble(t=t
-                , pY = pmin(pY_0*exp(r*t), 1)
-                ### round here???
-                , NY = round(N*pY)
-                # , NY = rbinom(pts, N, pY)
+                # , Y = 1-exp(-Ynull)
+                , Y = pmin(Ynull, 1)
+                , NY = round(N*Y)
+                # , NY = rbinom(pts, N, Y)
   )
-  if (max(sim$NY<dat$posTests)) cat("Underestimated infected population, positive tests exceed infected population", "\n")
-  if (max((N-sim$NY)<dat$negTests)) cat("Overestimated infected population, negative tests exceed uninfected population", "\n")
+  if (max(sim$NY<dat$posTests)) cat("Positive tests exceed infected population", "\n")
+  if (max((N-sim$NY)<dat$negTests)) cat("Negative tests exceed uninfected population", "\n")
   out <- (-sum(dbinom(dat$posTests, sim$NY, T_Y,log = TRUE))
           -sum(dbinom(dat$negTests,N-sim$NY,T_B,log = TRUE)))
   return(out)
 }
 
-real_ML<-LL(B,Phi,pY_0,r,dat,N,tmax)
+real_ML<-nll(B,Phi,log(Y_0),r,dat,N,tmax)
 real_ML
 
-LL(0.05,Phi,pY_0,0.2,dat,N,tmax)
+print(nll(B,Phi,log(0.5),0.0,dat,N,tmax))
 
-mle_out_debug <- try(mle2(LL
-                          ,start = list(B=0.06
-                                        ,Phi=Phi
-                                        ,pY_0=pY_0
-                                        ,r=0.2)
-                          ,data = list(dat=dat
-                                       ,N=N
-                                       ,tmax=tmax)
-                          ,control = list(maxit=10000)
-                          ,method = "Nelder-Mead"
-))
-warnings()
-r
-mle_out_debug
-# mle_out_NM <- update(mle_out, method = "Nelder-Mead")
-
-######### The optimization always leads to negative pY_0
-
-mle_out2 <- try(mle2(LL
+mle_out_debug <- try(mle2(nll
                           ,start = list(B=B
-                                        ,Phi=50
-                                        ,pY_0=9e-5
+                                        ,Phi=Phi
+                                        ,logY_0=log(Y_0)
                                         ,r=r)
                           ,data = list(dat=dat
                                        ,N=N
@@ -98,20 +83,6 @@ mle_out2 <- try(mle2(LL
                           ,control = list(maxit=10000)
                           ,method = "Nelder-Mead"
 ))
+warnings()
 
-mle_out2
-
-test_params <- list(B=B
-                    ,Phi=22
-                    ,pY_0=8e-5
-                    ,r=r)
-
-mledat <- list(dat=dat
-               ,N=N
-               ,tmax=tmax)
-
-do.call(LL, c(test_params, mledat))
-
-
-### Heatmap?
-
+print(mle_out_debug)
