aabef94ae5a97da865314ab59585968b63799a9c
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-03-17 14:26:27 -0400

    modified:   drbinom.R

--- drbinom.R
+++ drbinom.R
@@ -2,7 +2,8 @@
 ## This could be made more efficient with a purpose-built vectorization function; maybe if we are worried about performance at some point
 drbinom0 <- function(x, size, prob, log = TRUE){
 	if (x<=size) return(dbinom(x, size, prob, log))
-	return(dbinom(size, size, prob^(x/size), log))
+	if (log) return(x*log(prob))
+	return(prob^x)
 }
 
 drbinom <- Vectorize(drbinom0, c("x", "size", "prob"))
@@ -10,4 +11,9 @@ drbinom <- Vectorize(drbinom0, c("x", "size", "prob"))
 data.frame(
 	fragile=dbinom(1:13, 10, 0.8, log=TRUE)
 	, robust=drbinom(1:13, 10, 0.8)
+)
+
+data.frame(
+	fragile=dbinom(1:13, 10, 0.8)
+	, robust=drbinom(1:13, 10, 0.8, log=FALSE)
 )
